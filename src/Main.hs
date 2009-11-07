22e548b60ea6221d76b605b7bf06e0598d136008
Antti Salonen <ajsalonen@gmail.com>
2009-11-07 20:06:34 +0100

    enhanced browsing

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ import Text.Printf
 import qualified Data.Map as M
 
 import DataCreate
+import DataFunction
 import GalaxyStats
 import DataTypes
 import ZipperGalaxy
@@ -18,27 +19,24 @@ main = do
 
 type Input a = Maybe (GalaxyZipper a)
 
-sstitle :: Int -> StarSystem a -> String
-sstitle num ss = printf "%4d. %s - %d stars - %s" num (name ss) (M.size (stars ss)) ((show . ssposition) ss)
-
 getInput :: GalaxyZipper a -> IO (Input a)
 getInput z = do
   c <- getLine
   case reads c of
-    [(num, _)] -> case tryDown num z of
-                    Just nz -> return $ Just nz
-                    Nothing -> getInput z
+    [(num, _)] -> if num == 0 
+                    then return $ Just (up z) 
+                    else case tryDown num z of
+                           Just nz -> return $ Just nz
+                           Nothing -> getInput z
     _          -> if not (null c) && head c == 'q' 
                     then return Nothing 
                     else getInput z
 
-ssFromGal g =
-  let ss = (M.elems . starsystems) g
-      titles = zipWith sstitle [1..] ss
-  in concatMap (++ "\n") titles
-
 genInfo :: GalaxyZipper a -> String
-genInfo z = galaxyInfo z ++ starSystemInfo z ++ starInfo z ++ satelliteInfo z
+genInfo z = galaxyInfo z ++ 
+  (smallInfo z starSystemInZipper starSystemInfo) ++
+  (smallInfo z starInZipper starInfo) ++
+  (smallInfo z satelliteInZipper satelliteInfo)
 
 galaxyInfo :: GalaxyZipper a -> String
 galaxyInfo z = gname ++ "\n" ++ sysinfo
@@ -46,26 +44,48 @@ galaxyInfo z = gname ++ "\n" ++ sysinfo
         gname   = "Galaxy name: " ++ name g
         sysinfo = case starSystemInZipper z of
                     Just _  -> ""
-                    Nothing -> ssFromGal g
+                    Nothing -> getXInfoFromY g starsystems (genTitle sstitle)
 
-starSystemInfo z = 
-  let mss = starSystemInZipper z
-  in case mss of
+starSystemInfo ss z = sysinfo ++ "\n" ++ starinfo
+    where sysinfo = "Star system name: " ++ name ss
+          starinfo = case starInZipper z of
+                       Just _  -> ""
+                       Nothing -> getXInfoFromY ss stars (genTitle startitle)
+
+getXInfoFromY :: (Enum a, Num a) => b -> (b -> M.Map k c) -> (a -> c -> String) -> String
+getXInfoFromY y getfunc printfunc = 
+  let xs = (M.elems . getfunc) y
+      titles = zipWith printfunc [1..] xs
+  in concatMap (++ "\n") titles
+
+sstitle :: StarSystem a -> String
+sstitle ss = printf "%s - %d stars - %s" (name ss) (M.size (stars ss)) ((show . ssposition) ss)
+
+genTitle :: (a -> String) -> Int -> a -> String
+genTitle f num x = printf "%4d. %s" num (f x)
+
+startitle s = printf "%s - Type: %s - Temperature: %6d - %d planets" (name s) (show (spectralType s)) (temperature s) (M.size (planets s))
+
+smallInfo :: a -> (a -> Maybe b) -> (b -> a -> String) -> String
+smallInfo z getfunc printfunc = 
+  let mx = getfunc z
+  in case mx of
        Nothing -> ""
-       Just ss -> starSystemInfo' ss
+       Just x  -> printfunc x z
 
-starSystemInfo' ss = sysinfo ++ starinfo
-    where sysinfo = name ss
-          starinfo = ""
+starInfo s z = starinfo ++ "\n" ++ planetinfo
+  where starinfo = "Star name: " ++ name s
+        planetinfo = case satelliteInZipper z of
+                       Just _  -> ""
+                       Nothing -> getXInfoFromY s planets (genTitle infoSatellite)
 
-starInfo z = ""
-satelliteInfo z = ""
+satelliteInfo s z = ""
 
 infoSatellite :: Planet a -> String
 infoSatellite s = 
-  "Name: " ++ name s ++ "\n" ++
-  "Orbit radius: " ++ (show3f . orbitradius . orbit) s ++ "\n" ++
-  "Mass: " ++ (show3f . bodymass . physics) s ++ "\n" ++ 
+  "Name: " ++ name s ++ " - " ++
+  "Orbit radius: " ++ (show3f . orbitradius . orbit) s ++ " - " ++
+  "Mass: " ++ (show3f . bodymass . physics) s ++ " - " ++ 
   "Type: " ++ (show . planettype) s
 
 browseGalaxy g = do
@@ -73,5 +93,5 @@ browseGalaxy g = do
   i <- getInput g
   case i of
     Nothing -> return ()
-    Just x  -> return ()
+    Just nz -> browseGalaxy nz
 
